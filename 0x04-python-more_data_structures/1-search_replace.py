37460e43b766694474e375dc180b7c6e8eb36c01
RoseleneOdimgbe <babymesa98@gmail.com>
2024-01-10 18:45:58 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     copy = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
+    for i in my_list:
+        if i == search:
             copy.append(replace)
         else:
-            copy.append(my_list[i])
+            copy.append(i)
     return (my_list)
