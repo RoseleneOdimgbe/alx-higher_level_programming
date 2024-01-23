970e9ab44c0a60f613f1e283773ba5e6014aa7e0
RoseleneOdimgbe <babymesa98@gmail.com>
2024-01-22 18:14:13 -0800

    Python.h

---
+++ 0x05-python-exceptions/Python.h
@@ -0,0 +1,9 @@
+#ifndef Python_H
+#define Python_H
+#include <stdio.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif
