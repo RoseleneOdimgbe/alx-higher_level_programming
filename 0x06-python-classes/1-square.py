dd4db1b9edda263b472eec50dcea31b9b980379d
RoseleneOdimgbe <babymesa98@gmail.com>
2024-01-23 19:53:22 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""The square module"""
+
+
+class Square:
+    """This defines a square"""
+
+    def __init__(self, size):
+        """Constructor
+        Args:
+            size: this is the length of side of the square
+        """
+        self.__size = size
