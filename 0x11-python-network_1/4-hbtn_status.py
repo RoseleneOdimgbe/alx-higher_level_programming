5fb733aae362df30eb3c7eb5854af8cc6782c82b
RoseleneOdimgbe <babymesa98@gmail.com>
2024-04-26 19:57:11 -0700

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This imports requests"""
+
+import requests
+
+if __name__ == "__main__":
+
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+
+    print("Body response:")
+
+    print("\t- type: {}".format(type(r.text)))
+
+    print("\t- content: {}".format(r.text))
